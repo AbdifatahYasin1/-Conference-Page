17cf2d3e6c9704c14ed78893fe8f1082fc6eac92
AbdifatahYasin1 <iskatag123@gmail.com>
2022-10-27 11:14:27 +0300

    Added Hamburger menu

--- App.js
+++ App.js
@@ -1,3 +1,23 @@
+const header = document.querySelector(".header");
+// const section = document.querySelector(".Section-1");
+// document.section.append(header);
+
+//hanburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((navLink) =>
+  navLink.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
 const cardContainer = document.querySelector(".cardContainer");
 
 const Infomation = [
@@ -61,5 +81,3 @@ function createSpeaker(item) {
 Infomation.forEach((item) => {
   createSpeaker(item);
 });
-
-const header = document.querySelector(".header");
